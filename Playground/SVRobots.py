54acc331cbc120b63b61cce7934f61dd636ee77c
Richard Weddigen <SVRobots@users.noreply.github.com>
2014-02-09 20:27:50 -0500

    auto commit script

--- Playground/SVRobots.py
+++ Playground/SVRobots.py
@@ -6,6 +6,8 @@ from pyglet import *
 
 key = pyglet.window.key
 
+objects=[]
+
 class ElloWindow(pyglet.window.Window):
 	def __init__(self):
 		super(ElloWindow, self).__init__()
@@ -13,21 +15,44 @@ class ElloWindow(pyglet.window.Window):
 		#self.vsync=True #prevents tearing
 		self.label = pyglet.text.Label('Ello World!')
 		self.push_handlers(self.on_key_press)
-		self.x=30.0
-		self.y=35.0
+		self.tri=triangle()
+		self.controlindex=len(objects[0])-1
 	def on_draw(self):
 		self.clear()
-		self.label.draw()
-		pyglet.graphics.draw(3, pyglet.gl.GL_TRIANGLES, ('v3f', (self.x-50, self.y, 0.0, self.x, self.y, 0.0, self.x, self.y+50, 0.0)), ('c3B', (255,0,0,0,0,255,0,255,0)))
-	def on_key_press(self, symbol, modifiers):
+		i=0
+		for c in objects[0]:
+			if i==self.controlindex:
+				pyglet.graphics.draw(3, pyglet.gl.GL_TRIANGLES, ('v3f', (c.x-50, c.y, 0.0, c.x, c.y, 0.0, c.x, c.y+50, 0.0)), ('c3B', (255,0,0,0,0,255,0,255,0)))
+			else:
+				pyglet.graphics.draw(3, pyglet.gl.GL_TRIANGLES, ('v3f', (c.x-50, c.y, 0.0, c.x, c.y, 0.0, c.x, c.y+50, 0.0)), ('c3B', (255,255,255,255,255,255,255,255,255)))
+			i=i+1
+	def on_key_press(self, symbol, modifier):
 		if symbol == key.W:
-			self.y=self.y+10
+			objects[0][self.controlindex].y=objects[0][self.controlindex].y+10
 		if symbol == key.S:
-			self.y=self.y-10
+			objects[0][self.controlindex].y=objects[0][self.controlindex].y-10
 		if symbol == key.D:
-			self.x=self.x+10
+			objects[0][self.controlindex].x=objects[0][self.controlindex].x+10
 		if symbol == key.A:
-			self.x=self.x-10
+			objects[0][self.controlindex].x=objects[0][self.controlindex].x-10
+		if symbol == key.ENTER:
+			self.tri = triangle()
+			self.controlindex=len(objects[0])-1
+		if symbol == key.UP:
+			if self.controlindex < len(objects[0])-1:
+				self.controlindex=self.controlindex+1
+		if symbol == key.DOWN:
+			if self.controlindex != 0:
+				self.controlindex=self.controlindex-1
+
+class triangle(object):
+	ID=0
+	def __init__(self):
+		super(triangle, self)
+		objects[self.ID].append(self)
+		self.name='Triangle'
+		self.x=100
+		self.y=100
 
 def start_game():
 	def on_key_press(symbol, modifiers):
@@ -44,6 +69,8 @@ def end_game():
 	window.pop_handlers()
 
 if __name__ == '__main__':
+	#Declare Triangles
+	objects.append([])
 	window = ElloWindow()
 	#start_game()
 	pyglet.app.run()
